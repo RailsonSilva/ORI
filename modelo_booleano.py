dc087ea3a50ff032bd2ec5e467130141509a845d
Railson da Silva Martins <84814735+RailsonSilva@users.noreply.github.com>
2021-09-11 20:50:56 -0300

    Add files via upload
    11/09/2021

--- modelo_booleano.py
+++ modelo_booleano.py
@@ -1,6 +1,6 @@
 # RAILSON DA SILVA MARTINS
 # 11811BSI208
-# 
+# CONCLUIDO DIA 10/09 
 
 from os import close
 import nltk
@@ -95,7 +95,7 @@ for c in palavras: #for na lista das palavras criada em ordem ascii
         if c in indice[i]: # se a palavra existe no indice imprime qual o indice e quantas vezes apareceu
             arquivo.write("{},{} " .format(i, indice[i][c]))
             if i == 1:
-                indice1.add(c)
+                indice1.add(c) #armazena as palavras que estão no indice invertido informado
             if i == 2:
                 indice2.add(c)
             if i == 3:
@@ -108,26 +108,113 @@ arquivo.close() #fecha arquivo indice.txt
 consulta = open("consulta.txt") # lê o arquivo 
 consulta_read = consulta.readline() #lê a linha do arquivo
 consulta_read = consulta_read.lower()
-consulta_read_filter = consulta_read.split() #
+consulta_read_filter = consulta_read.split() # separa a frase por palavra
 
+tem_ou = 0
 i = 0
 while i < len(consulta_read_filter):
         if consulta_read_filter[i][0] == '!' or consulta_read_filter[i].isalpha(): #identifica se é letra ou palavra negativa
             stemmer = nltk.stem.RSLPStemmer() #retira o radical da palavra
             consulta_read_filter[i] = stemmer.stem(consulta_read_filter[i]) #salva na variável
+        if consulta_read_filter[i] == "|": 
+            tem_ou = 1
         i = i + 1
 
-print("aqui")
-print(consulta_read_filter) # imprime - ['cas', '&', 'am', '|', 'cas', '&', '!mor']
-print("aqui")
+consulta_read_filter_radical = " ".join(consulta_read_filter) # junta novamente a string já com o radical da palavra
 
-j = 0
-while j < len(consulta_read_filter):
 
-    if(consulta_read_filter[j] in ("&")):
-        if(consulta_read_filter[j-1] and consulta_read_filter[j+1] in palavras):
-            print(consulta_read_filter[j-1])
-            print(consulta_read_filter[j+1])
+def verifica_E_e_NOT(consulta):
+    s1 = consulta
+    s1 = s1.replace(" ", "") # remove espaços da string 
+    s1 = s1.split("&") #separa as strings pelo operador &
+    
+    i1 = 0
+    i2 = 0
+    i3 = 0
+    conjunto = set()
+    c2 = set()
 
-            
-    j = j + 1
+    for c in s1: #checa se tem alguma palavra com not e retira o "!" e faz uma lista de palavras que tem not
+        if c[0] == "!":
+            c1 = c.split("!")
+            c1 = c1[1]
+            c2.add(c1) #adiciona todas as palavras negativas encontradas
+
+    for c in s1:
+        if len(c2) > 0:
+            tem = 0
+            if c[0] == "!":
+                c = c.split("!")
+                c = c[1]
+                tem = 1
+
+            if tem == 1:
+                if c not in indice1:
+                    i1 = i1 + 1
+                    if(i1 == len(s1)) or len(s1) == 1:
+                        conjunto.add("a.txt")
+                if c not in indice2:
+                    i2 = i2 + 1
+                    if(i2 == len(s1)) or len(s1) == 1:
+                        conjunto.add("b.txt")
+                if c not in indice3:
+                    i3 = i3 + 1
+                    if(i3 == len(s1)) or len(s1) == 1:
+                        conjunto.add("c.txt")
+            else:
+                if c in indice1:
+                    i1 = i1 + 1
+                    if(i1 == len(s1)) or len(s1) == 1:
+                        conjunto.add("a.txt")
+                if c in indice2:
+                    i2 = i2 + 1
+                    if(i2 == len(s1)) or len(s1) == 1:
+                        conjunto.add("b.txt")
+                if c in indice3:
+                    i3 = i3 + 1
+                    if(i3 == len(s1)) or len(s1) == 1:
+                        conjunto.add("c.txt")
+        else:
+            if c in indice1:
+                i1 = i1 + 1
+                if(i1 == len(s1)) or len(s1) == 1:
+                    conjunto.add("a.txt")
+            if c in indice2:
+                i2 = i2 + 1
+                if(i2 == len(s1)) or len(s1) == 1:
+                    conjunto.add("b.txt")
+            if c in indice3:
+                i3 = i3 + 1
+                if(i3 == len(s1)) or len(s1) == 1:
+                    conjunto.add("c.txt")
+    return conjunto
+
+if tem_ou == 1:
+    consulta_read_filter_radical = consulta_read_filter_radical.split("|") #separa as frases pelo operador OU
+else:
+    consulta_read_filter_radical = consulta_read_filter_radical.split("  ") #deixa a consulta inteira como apenas uma consulta se não tiver operador OU
+
+consulta_e_not_conj = set()
+i = 0
+while i < len(consulta_read_filter_radical):
+    consulta_e_not = dict() # determina como dicioário
+    consulta_e_not[i+1] = verifica_E_e_NOT(consulta_read_filter_radical[i]) # chama função e armazena o valor na variavel
+    consulta_e_not[i+1] = " ".join(consulta_e_not[i+1]) #transforma em string
+    consulta_e_not_conj.add(consulta_e_not[i+1]) #adiciona ao conjunto evitando repetições de documentos
+    i = i + 1
+
+consulta_e_not_conj = " ".join(consulta_e_not_conj) # transforma em string 
+consulta_e_not_conj = consulta_e_not_conj.split() # transforma em lista
+consulta_aparece = len(consulta_e_not_conj) # verifica quantas vezes a consulta é verdadeira
+consulta_e_not_conj = set(consulta_e_not_conj) #trasnforma em conjunto impedindo a repitição para a impressão
+consulta_e_not_conj = sorted(consulta_e_not_conj) #coloca em ordem ASCII
+
+# --------------------------------------------------
+# gerando arquivo 
+arquivo_resposta = open("resposta.txt", "w") #cria o arquivo
+arquivo_resposta.write("{}" .format(consulta_aparece))
+arquivo_resposta.write("\n")# adiciona ENTER
+for c in consulta_e_not_conj: # for dentro 
+    arquivo_resposta.write("{}" .format(c)) # imprime os arquivos que fazem a conjunção verdadeira
+    arquivo_resposta.write("\n")# adiciona ENTER
+arquivo_resposta.close() #fecha arquivo resposta.txt
